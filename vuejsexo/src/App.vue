c43f912d68f997921b3358c1960085fc687b3c14
jade alfaiao <jade.alfaiao@gmail.com>
2023-03-14 15:57:32 +0100

    to do list premiere version

---
+++ vuejsexo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div>My little to do list</div>
+    <todo-list />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+export default {
+  name: "app",
+  components: {
+    TodoList
+  }
+};
+</script>
+
+<style>
+#app {
+  color: #2c3e50;
+  margin-top: 5px;
+  background-color: #56b0ee;
+}
+</style>
